dfea3d94c52e1e3c6b104dce30696021890943f6
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-24 11:55:45 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 const App = () => {
   return (
     <div>
--- src/hooks/usestate/handler.jsx
+++ src/hooks/usestate/handler.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
  
 const Handler = () => {
     let [msg,setmsg]=useState("hello")
@@ -13,7 +14,7 @@ const Handler = () => {
     <div>
 
         <h1>{msg}</h1>
-        <button className="btn btn-danger" onClick={updateva.bind(null,"gm")}>gm</button>
+        <button className="btn btn-warning"  onClick={updateva.bind(null,"gm")}>gm</button>
         <button onClick={updateva.bind(null,"ga")}>ga</button>
         <button onClick={updateva.bind(null,"ge")}>ge</button>
         <button onClick={updateva.bind(null,"gn")}>gn</button>
