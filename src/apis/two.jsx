82d7231919ec838f8680ef3d57e804e67713828b
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-30 12:29:57 +0530

    PATH

--- src/apis/two.jsx
+++ src/apis/two.jsx
@@ -1,21 +1,59 @@
-import {useState,useEffect } from 'react'
-import React from 'react'
-import Axios   from 'axios'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Fetchapi = () => {
 
+  const [posts, setPosts] = useState([])
 
-    let [data,setdata]=useState([])
-    useEffect(()=>{
-        Axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then((res)=>setdata(res.data))
-        //.then((catch)=>console.log(catch))
-    })
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div>
+      <h3>Posts</h3>
 
-        <pre>{data}</pre>
-      
+      {/* Debug */}
+      <pre>{JSON.stringify(posts)}</pre>
+      {
+        posts.length>0 ?  <div>
+            <thead>
+                <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>body</th>
+                </tr>
+ 
+            </thead>
+            
+                {posts.length > 0 ? <div>
+                     <tbody>
+                        {posts.map(()=>{
+                            return (
+                                <tr key=index>
+                                <td>{posts.id}</td>
+                    <td>{posts.title}</td>
+                    <td>{posts.body}</td>
+                
+
+                                </tr>
+                            )
+                        })
+
+                        }
+                        <tr>
+                     
+                     </tbody>
+
+                </div>}
+                 
+           
+        </div>: <h3>No Data</h3>
+      }
+
+      <h1>hello</h1>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { createRoot } from 'react-dom/client'
 //import Handler from './hooks/usestate/handler'
 //import Message from './hooks/usestate/clsstate'
 
-import One from './apis/one.jsx'
+
+//import One from './apis/one.jsx'
+
+import Fetchapi from './apis/two'
 
 createRoot(document.getElementById('root1')).render(
   <StrictMode>
 
-    <One/>
+    <Fetchapi/>
  
  
    
